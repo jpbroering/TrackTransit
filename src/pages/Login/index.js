56459ffb29d416e42c9d15f256d0d2e039070d2d
jpbroering <102531849+jpbroering@users.noreply.github.com>
2023-10-11 09:01:21 -0300

    criei a pagina login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,112 @@
 import 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image, TextInput, TouchableOpacity, Text, Svg } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 
-export default function Login(){
+export default function Logingn(){
     const navigation = useNavigation()
     return(
-        <View>
-
+        <View style={ styles.container } >
+            <View style={ styles.containerHeader }>
+                <Image source={require('../../assets/tLogo.png')} />
+            </View>
+            <View style={ styles.containerForm }>
+                <TextInput placeholder='email' style={styles.input}/>
+                <TextInput placeholder='senha' secureTextEntry={true} style={styles.input}/>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={ styles.containerBottom}>
+                <Text style={styles.message}>ou continue com</Text>
+                <View style={styles.containerCollumn}>
+                  <View style={styles.containerButton}>
+                    <TouchableOpacity>
+                      <Image style={styles.icon} source={require('../../assets/googleIcon.png')}/>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.containerButton}>
+                    <TouchableOpacity >
+                      <Image style={styles.icon} source={require('../../assets/facebookIcon.png')}/>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.containerButton}>
+                    <TouchableOpacity >
+                      <Image style={styles.icon} source={require('../../assets/appleIcon.png')}/>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                <Text style={styles.registerText}>Não possui uma conta? <Text style={styles.textBold}>Cadastre-se</Text></Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    containerHeader: {
+        alignItems: 'center'
+    },
+    containerForm: {
+      alignItems: 'center'
+    },
+    containerBottom: {
 
+    },
+    containerCollumn: {
+      display: 'flex',
+      flexDirection: 'row',
+      padding: 12,
+      justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#fff',
+        width: '30%',
+        borderRadius: 20,
+        paddingVertical: 8,
+        marginTop: '5%',
+        alignItems: 'center'
+      },
+    buttonText: {
+      color: '#000000',
+      fontSize: 18,
+      fontWeight: 'bold'
+    },
+      registerText: {
+        color: '#fff',
+        textAlign: 'center'
+      },
+      textBold: {
+        fontWeight: 'bold'
+      },
+      message: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center'
+      },
+      input: {
+        marginTop: '3%',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        width: '70%',
+        paddingLeft: 20,
+        paddingRight: 20,
+        height: 35,
+        borderRadius: 50,
+      },
+      icon: {
+        width: 36,
+        height: 44,
+        borderRadius: 5
+      },
+      containerButton: {
+        alignItems: 'center',
+        marginRight: '5%',
+        marginLeft: '5%'
+      }
 })
